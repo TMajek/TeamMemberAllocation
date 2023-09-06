94da3f758b658a9e0caabf288c2ea4addfedcdcb
TMajek <96519099+TMajek@users.noreply.github.com>
2023-09-06 13:39:59 +0000

    css add

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,13 @@
 const Header = ({selectedTeam, teamMemberCount}) => {
 
   return (
-    <header>
-      <h1 className=''>Team Member Allocation : {teamMemberCount}</h1>
-      <h2>Current Team: {selectedTeam}</h2>
+    <header className= 'containter'>
+      <div className='row justify-content-center mt-3 mb-4'>
+        <div className = 'col-8'>
+          <h1>Team Member Allocation : {teamMemberCount}</h1>
+          <h2>Current Team: {selectedTeam}</h2>
+        </div>
+      </div>
     </header>
   )
 
