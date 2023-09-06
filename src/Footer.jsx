48fb0cbc19daa1f5ec8cc8851906b9a5024f9ed6
TMajek <96519099+TMajek@users.noreply.github.com>
2023-09-06 13:47:00 +0000

    Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,12 @@
 const Footer = () => {
 
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className = 'container'>
+      <div className='row justify-content-center mt-3 mb-4'>
+        <div className = 'col-8'>
+          <h5>Team Member allocation app</h5>
+        </div>
+        </div>
     </footer>
   )
 
